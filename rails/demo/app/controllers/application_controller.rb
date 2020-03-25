b5339445117d52d69a515f864baffe13576f1984
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-03-25 16:10:38 +0530

    Some debugging remaining

--- rails/demo/app/controllers/application_controller.rb
+++ rails/demo/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+    def current_cart
+      Cart.find([:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
--- rails/demo/db/schema.rb
+++ rails/demo/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_111327) do
+ActiveRecord::Schema.define(version: 2020_03_25_085752) do
+
+  create_table "carts", force: :cascade do |t|
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
